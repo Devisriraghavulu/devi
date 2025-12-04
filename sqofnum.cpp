29be41305186237f581ec63b15c96524d8218944
Devisriraghavulu <srisaibalaji243@gmail.com>
2025-12-04 20:17:25 +0530

    Add files via upload

---
+++ sqofnum.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+int square(int n) { 
+return n * n; 
+} 
+int main() { 
+int num; 
+printf("Enter a number: "); 
+scanf("%d", &num); 
+printf("Square = %d\n", square(num)); 
+return 0; 
+}
